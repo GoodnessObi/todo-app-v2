10c3dbe1c20ba15bb65f2ddda5305a0e848828a7
GoodnessObi <goodnessobi@gmail.com>
2021-08-31 21:35:05 +0100

    allow user to add a new item to list

--- src/components/list/AddItemForm.js
+++ src/components/list/AddItemForm.js
@@ -1,14 +1,35 @@
-import React from 'react'
+import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 const AddItemForm = ({ addItem }) => {
+  const [todo, setTodo] = useState('')
+
   const saveItem = () => {
-    console.log('save');
-    addItem();
+    if (todo === '') {
+      console.log('nope')
+    } else {
+      const newTodo = {
+        todo,
+        id: uuidv4(),
+        date: new Date()
+      }
+      console.log(newTodo, 'new')
+      addItem(newTodo);
+      setTodo('')
+    }
+    
   }
 
   return (
     <div className='enter-input'>
-      <input type="text" id="input-box" placeholder="Add to your list . . ." />
+      <input 
+        type="text"
+        name="todo"
+        value={todo}
+        id="input-box"
+        onChange={e => setTodo(e.target.value)}
+        placeholder="Add to your list . . ."
+        />
       <button id="add-to-list" onClick={saveItem}>Add</button>
     </div>
   )
